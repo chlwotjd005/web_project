9460bc57202a8925238456214bc1217ae04a869a
hongchal <chal405@naver.com>
2020-11-26 01:43:04 +0900

    merge files and make server by Hongchal

---
+++ server.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var fs = require('fs');
+// var path = require('path');
+// var bodyParser = require('body-parser');
+
+var app = express();
+var path = require('path');
+
+
+app.use(express.static('main'));
+app.use(express.static(path.join(__dirname,'public')));
+
+
+app.get('/productlist_page/productlist_with_mainframe/productlist_with_mainframe.html', function(req, res){
+    fs.readFile('./productlist_page/productlist_with_mainframe/productlist_with_mainframe.html', function(error, data){
+        res.writeHead(200, {'Contesnt-type': 'text/html' });
+        res.end(data);
+    });
+});
+
+app.get('/login/login.html', function(req, res){
+    fs.readFile('./login/login.html', function(error, data){
+        res.writeHead(200, {'Contesnt-type': 'text/html' });
+        res.end(data);
+    });
+});
+
+app.get('/login/signup.html', function(req, res){
+    fs.readFile('/login/signup.html', function(error, data){
+        res.writeHead(200, {'Contesnt-type': 'text/html' });
+        res.end(data);
+    });
+});
+
+
+app.get('/productlist_page/productlist_with_mainframe/productlist_for_category.html', function(req, res){
+    fs.readFile('./productlist_page/productlist_with_mainframe/productlist_for_category.html', function(error, data){
+        res.writeHead(200, {'Contesnt-type': 'text/html' });
+        res.end(data);
+    });
+});
+
+
+app.listen(52273, function () {
+    console.log('Server Running at http://127.0.0.1:52273');
+});
+
