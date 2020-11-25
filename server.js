8d205cbcd7cf942668eddd32ea035d68680b1687
hongchal <chal405@naver.com>
2020-11-26 03:03:12 +0900

    update main to gategory move by HC

--- server.js
+++ server.js
@@ -33,13 +33,63 @@ app.get('/login/signup.html', function(req, res){
 });
 
 
-app.get('/productlist_page/productlist_with_mainframe/productlist_for_category.html', function(req, res){
-    fs.readFile('./productlist_page/productlist_with_mainframe/productlist_for_category.html', function(error, data){
+app.get('/productlist_page/productlist_with_mainframe/productlist_for_clothes.html', function(req, res){
+    fs.readFile('./productlist_page/productlist_with_mainframe/productlist_for_clothes.html', function(error, data){
         res.writeHead(200, {'Contesnt-type': 'text/html' });
         res.end(data);
     });
 });
 
+app.get('/productlist_page/productlist_with_mainframe/productlist_for_top.html', function(req, res){
+    fs.readFile('./productlist_page/productlist_with_mainframe/productlist_for_top.html', function(error, data){
+        res.writeHead(200, {'Contesnt-type': 'text/html' });
+        res.end(data);
+    });
+});
+
+app.get('/productlist_page/productlist_with_mainframe/productlist_for_shose.html', function(req, res){
+    fs.readFile('./productlist_page/productlist_with_mainframe/productlist_for_shose.html', function(error, data){
+        res.writeHead(200, {'Contesnt-type': 'text/html' });
+        res.end(data);
+    });
+});
+
+app.get('/productlist_page/productlist_with_mainframe/productlist_for_Book1.html', function(req, res){
+    fs.readFile('./productlist_page/productlist_with_mainframe/productlist_for_Book1.html', function(error, data){
+        res.writeHead(200, {'Contesnt-type': 'text/html' });
+        res.end(data);
+    });
+});
+app.get('/productlist_page/productlist_with_mainframe/productlist_for_Book4.html', function(req, res){
+    fs.readFile('./productlist_page/productlist_with_mainframe/productlist_for_Book4.html', function(error, data){
+        res.writeHead(200, {'Contesnt-type': 'text/html' });
+        res.end(data);
+    });
+});
+app.get('/productlist_page/productlist_with_mainframe/productlist_for_phone.html', function(req, res){
+    fs.readFile('./productlist_page/productlist_with_mainframe/productlist_for_phone.html', function(error, data){
+        res.writeHead(200, {'Contesnt-type': 'text/html' });
+        res.end(data);
+    });
+});
+app.get('/productlist_page/productlist_with_mainframe/productlist_for_etc.html', function(req, res){
+    fs.readFile('./productlist_page/productlist_with_mainframe/productlist_for_etc.html', function(error, data){
+        res.writeHead(200, {'Contesnt-type': 'text/html' });
+        res.end(data);
+    });
+});
+app.get('/productlist_page/productlist_with_mainframe/productlist_for_drink.html', function(req, res){
+    fs.readFile('./productlist_page/productlist_with_mainframe/productlist_for_drink.html', function(error, data){
+        res.writeHead(200, {'Contesnt-type': 'text/html' });
+        res.end(data);
+    });
+});
+app.get('/productlist_page/productlist_with_mainframe/productlist_for_oneroom.html', function(req, res){
+    fs.readFile('./productlist_page/productlist_with_mainframe/productlist_for_oneroom.html', function(error, data){
+        res.writeHead(200, {'Contesnt-type': 'text/html' });
+        res.end(data);
+    });
+});
 
 app.listen(52273, function () {
     console.log('Server Running at http://127.0.0.1:52273');
